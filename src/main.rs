c30cbf921ca2945ef27abeba6cc2c7b720e2d19d
Harald Hoyer <harald@profian.com>
2022-03-09 16:13:33 +0100

    iii
    Signed-off-by: Harald Hoyer <harald@profian.com>

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    use std::{thread, time};
+
+    let ten_secs = time::Duration::from_secs(10);
+    let now = time::Instant::now();
+
+    thread::sleep(ten_secs);
+
+    assert!(now.elapsed() >= ten_secs);
+
+    println!("Hello, world!");
+}
